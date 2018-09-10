f8f5625ef9ee9a0864e1acbd13c34fe254cd0903
Luis Javier Cabello de Alba Fernandez <luisjavier.cabellodealba@bbva.com>
2018-09-10 07:57:06 +0200

    User model created

---
+++ models/user.js
@@ -0,0 +1,17 @@
+
+'use strict';
+
+let mongoose = require('mongoose');
+let Schema = mongoose.Schema;
+
+
+let UserSchema = Schema({
+  name: String,
+  surname: String,
+  email: String,
+  password: String,
+  rol: String,
+  image: String
+});
+
+module.exports = mongoose.model('User', UserSchema);
