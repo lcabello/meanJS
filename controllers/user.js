d99b448440315e1106c72aafee65c2034eb9d931
Luis Cabello de Alba Fernandez <luisjavier.cabellodealba@bbva.com>
2018-09-19 20:28:59 +0200

    Save user method created

--- controllers/user.js
+++ controllers/user.js
@@ -1,9 +1,62 @@
+const bcrypt = require('bcrypt-nodejs');
+const User = require('../models/user');
+
 function pruebas (req, res) {
   res.status(200).send({
     message: 'Trying user controller'
   });
 }
 
+function saveUser (req, res) {
+  const user = new User();
+  const params = req.body || {};
+
+  if (params && !params.password) {
+    res.status(500).send({
+      message: 'No password'
+    });
+  } else {
+    user.name = params.name;
+    user.surname = params.surname;
+    user.email = params.email;
+    user.role = 'ROLE_USER';
+    user.image = 'null';
+
+    bcrypt.hash(params.password, null, null, (err, hash) => {
+      if (err) {
+        res.status(500).send({
+          message: 'Problem saving data'
+        });
+      }
+      user.password = hash;
+      if (user.name !== null && user.email !== null) {
+        // Saving user
+        user.save((errSave, userStored) => {
+          if (errSave) {
+            res.status(500).send({
+              message: 'Problem saving data'
+            });
+          } else {
+            res.status(200).send({
+              message: 'User saved',
+              user: userStored
+            });
+          }
+        });
+      } else {
+        res.status(500).send({
+          message: 'No correct info'
+        });
+      }
+    });
+
+    res.status(200).send({
+      message: 'OK password'
+    });
+  }
+}
+
 module.exports = {
-  pruebas
+  pruebas,
+  saveUser
 };
--- routes/user.js
+++ routes/user.js
@@ -4,5 +4,6 @@ const UserController = require('../controllers/user');
 const api = express.Router();
 
 api.get('/controller-user', UserController.pruebas);
+api.post('/register', UserController.saveUser);
 
 module.exports = api;
